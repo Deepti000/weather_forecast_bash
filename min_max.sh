aa81b1405665526bc2e9f7d5521e8ac3891f001f
Deepti000 <65061604+Deepti000@users.noreply.github.com>
2023-12-25 21:53:46 +0530

    Update min_max.sh

--- min_max.sh
+++ min_max.sh
@@ -2,28 +2,34 @@
 
 wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-LX0117EN-Coursera/labs/synthetic_historical_fc_accuracy.tsv
 
-arr=$(cat synthetic_historical_fc_accuracy.tsv | cut -d " " -f6)
+arr=$(cat synthetic_historical_fc_accuracy.tsv | cut -d "       " -f6)
+count=$(cat synthetic_historical_fc_accuracy.tsv | wc -l)
+fin=$((count-1))
+echo $arr
 
-for i in $(!arr[@])
+for ((i=1;i<=fin;i++))
 do
-   if [[ arr[i] < 0 ]]
+   if [[ ${arr[$i]} < 0 ]]
    then
-        arr[i] = (-1)*arr[i]
+           value=${arr[$i]}
+           arr[$i]=$((-1 * value))
+           echo $value
    fi
 done
-min=arr[0]
-max=arr[0]
+mins=${arr[1]}
+max=${arr[1]}
 
-for i in $(!arr[@])
+for ((i=1;i<=fin;i++))
 do
-   if [[ arr[i] < min ]]
+   if [[ ${arr[$i]} < $mins ]]
    then
-        min = arr[i]
-   elif [[ arr[i]>max ]]
+           mins=$((arr[$i]))
+   fi
+   if [[ ${arr[$i]} > $max ]]
    then
-        max = arr[i]
+           max=$((arr[$i]))
    fi
 done
 
-echo "minimum ebsolute error = $min"
+echo "minimum ebsolute error = $mins"
 echo "maximum absolute error = $max"
