bcf9105cb16e2aaa5dfe00543589cde4855421a5
Deepti000 <65061604+Deepti000@users.noreply.github.com>
2023-12-25 20:51:31 +0530

    Update rx_poc.sh

--- rx_poc.sh
+++ rx_poc.sh
@@ -2,7 +2,7 @@
 
 $city="Casablanca"
 
-curl  wttr.in/$city >> weather_report
+curl  wttr.in/$city?T >> weather_report
 cur=$(cat weather_report | grep °C | head -n 1 | awk {'print $(NF-1)'})
 tom=$(cat weather_report |head -n 23 | tail -n 1 | cut -d "C" -f2 | awk '{print $(NF -1)}')
 hour=$(TZ='Morocco/Casablanca' date -u +%H)
